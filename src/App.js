3430f2f56cb8475341f44dfb23e2acf04cbbcb2a
matthias haefeli <haefeli.matthias@gmail.com>
2020-10-13 10:01:48 -0600

    form submit function

--- src/App.js
+++ src/App.js
@@ -13,6 +13,31 @@ const initalExpenses = [
 
 function App() {
   const [expenses, setExpenses] = useState(initalExpenses)
+  const [charge, setCharge] = useState('')
+  const [amount, setAmount] = useState('')
+
+  const handleCharge = e => {
+    setCharge(e.target.value)
+  }
+
+  const handleAmount = e => {
+    setAmount(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (charge !== '' && amount > 0) {
+      const singleExpense = {id: uuid(), charge, amount}
+      setExpenses([...expenses, singleExpense])
+      setCharge('')
+      setAmount('')
+    } else {
+      // handle alert
+    }
+
+
+    
+  }
 
   const handleDelete = () => {
     setExpenses([])
@@ -23,13 +48,18 @@ function App() {
       <Alert />
       <h1>budget calculator</h1>
       <main className='App'>
-        <Form />
+        <Form
+          charge={charge}
+          amount={amount}
+          handleCharge={handleCharge}
+          handleAmount={handleAmount}
+          handleSubmit={handleSubmit} />
         <List expenses={expenses} handleDelete={handleDelete} />
       </main>
       <h1>total spending:
         <span className='total'>
           $ {expenses.reduce((acc, curr) => {
-            return acc += curr.amount
+            return acc += parseInt(curr.amount)
           }, 0)}
         </span>
       </h1>
