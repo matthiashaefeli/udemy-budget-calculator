2642f1bcf0bbac34daa17c8ee8aae1340ef1bfbf
matthias haefeli <haefeli.matthias@gmail.com>
2020-10-13 09:23:08 -0600

    item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import {MdEdit, MdDelete} from 'react-icons/md'
 
-const Item = () => {
+const Item = ({expense}) => {
+  const {id, charge, amount} = expense
+
   return (
-    <div>
-      item
-    </div>
+    <li className='item'>
+      <div className='info'>
+        <span className='expense'>{charge}</span>
+        <span className='amount'>${amount}</span>
+      </div>
+      <div>
+        <button className='edit-btn' arial-label='edit'>
+          <MdEdit />
+        </button>
+        <button className='clear-btn' arial-label='delete'>
+          <MdDelete />
+        </button>
+      </div>
+    </li>
   )
 }
 
